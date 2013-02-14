2676c182618488f14f3e28ff38d63d37fbbb4314
Pascal Lalancette <okcompute@me.com>
2013-02-14 17:06:10 -0500

    Delete empty folders from a root method added

--- p4clean.py
+++ p4clean.py
@@ -1,24 +1,19 @@
 import os
 import argparse
+import errno
 
-
-def is_empty_folder(folder):
-    """Return True if the folder is empty"""
-    pass
-
-
-def build_empty_folder_list(path):
-    """ Create a lists of all empty folder inside the path"""
-    pass
-
-
-def delete_empty_folders(empty_folders_list):
-    pass
-
-def delete_empty_folder(folder):
-    """Delete the folder only if it is found to be empty, otherwise
+def delete_empty_folders(root):
+    """Delete the folders only if it is found to be empty, otherwise
     do nothing"""
-    pass
+    for path, directories, files in os.walk(root, topdown=False):
+        print "path is being checked :files %d    dir %d " % (len(files), len(directories)) + path
+        if not files:
+            try:
+                print "deleting " + path
+                os.rmdir(path)
+            except OSError, e:
+                if e.errno == errno.ENOTEMPTY:
+                    pass
 
 
 def main():
@@ -27,14 +22,9 @@ def main():
     parser.add_argument("path", help="root path from which all child empty folders will be deleted")
     args = parser.parse_args()
 
-    for path, directory, file in os.walk(args.path, topDown=True):
-        try:
-            os.rmdir(os.path.join(path))
-        except OSError, e
-        if e.errno == errno.ENOTEMPTY:
-            pass
-        else:
-            print "%s folder delete." % path
+    # Remove any empty folders left by the clean up
+    delete_empty_folders(args.path)
+
 
 if __name__ == "__main__":
     main()
--- test/test_p4clean.py
+++ test/test_p4clean.py
@@ -1,17 +1,48 @@
 import pytest
-from p4-clean import delete_empty_folder
+import shutil
+import os
+import p4clean
 
-def test_delete_empty_folder():
-    """Test the function delete_empty_folder."""
-    # Preparation
-    empty_folder = os.tmpdir()
-    os.mkdir(empty_folder)
 
-    # The function to test
-    delete_emtpty_folder(emtpy_folder)
+def test_delete_empty_folders():
+    root_folder = os.tmpnam()
 
-    assert os.access("empty_folder", os.R_OK) "Folder should be empty"
+    # Create a folder tree
+    os.mkdir(root_folder)
+    os.mkdir(root_folder + '/folderA')
+    os.mkdir(root_folder + '/folderA/folderAA')
+    os.mkdir(root_folder + '/folderB')
+    os.mkdir(root_folder + '/folderC')
+    os.mkdir(root_folder + '/folderC/folderCC')
+    os.mkdir(root_folder + '/folderD')
+    os.mkdir(root_folder + '/folderD/folderDD')
 
-    os.rmdir(empty_folder)
+    def create_file(root, path):
+        temp_file_path = root + '/' + path
+        temp_file = open(temp_file_path, "wb")
+        temp_file.write("")
+        temp_file.close()
+
+    # populate random folders with one file
+    create_file(root_folder, 'folderA/temp.txt')
+    create_file(root_folder, 'folderA/folderAA/temp.txt')
+    create_file(root_folder, 'folderD/folderDD/temp.txt')
+
+    # the tested function call
+    p4clean.delete_empty_folders(root_folder)
+
+    folder_list = [path for path, directories, files in os.walk(root_folder)]
+
+    assert root_folder in folder_list, "Folder is not empty and should still  exist"
+    assert root_folder + '/folderA' in folder_list, "Folder is not empty and should still  exist"
+    assert root_folder + '/folderA/folderAA' in folder_list, "Folder is not empty and should still  exist"
+    assert root_folder + '/folderD' in folder_list, "Folder is not empty and should still  exist"
+    assert root_folder + '/folderD/folderDD' in folder_list, "Folder is not empty and should still  exist"
+    assert root_folder + '/folderB' not in folder_list, "Folder is empty and should not exist"
+    assert root_folder + '/folderC' not in folder_list, "Folder is empty and should not exist"
+    assert root_folder + '/folderC/folderCC' not in folder_list, "Folder is empty and should not exist"
+
+    shutil.rmtree(root_folder)
+
 
 pytest.main()
