ddfdae8525bcf813df17abc99ec57918276f9514
Pascal Lalancette <okcompute@me.com>
2013-03-13 07:03:44 -0400

    Add exclusion rules by args or config files

--- p4clean.py
+++ p4clean.py
@@ -2,6 +2,83 @@ import os
 import argparse
 import errno
 from commands import getoutput
+import re
+import fnmatch
+import ConfigParser
+import itertools
+import logging
+
+
+class P4CleanConfig(object):
+    """Configurations for processing the p4 depot clean up process."""
+
+    SECTION_NAME = 'p4clean'
+    EXCLUSION_OPTION = 'exclusion'
+
+    def __init__(self, path="", exclusion=""):
+        self.logger = logging.getLogger()
+        args_exclusion_list = exclusion.split(';')
+        config_exclusion_list = []
+
+        path = self.validate_config_file_path(path)
+        if path:
+            config_exclusion_list = self.parse_config_file(path)
+
+        # chain args and config file exclusion lists
+        exclusion_list = itertools.chain(args_exclusion_list,
+                                         config_exclusion_list)
+        exclusion_list = list(exclusion_list)
+        self.exclusion_regex = self.compute_regex(exclusion_list)
+
+    def compute_regex(self, exclusion_list):
+        return r'|'.join([fnmatch.translate(x) for x in exclusion_list]) or r'$.'
+
+    def is_excluded(self, filename):
+        return re.match(self.exclusion_regex, filename)
+
+    def validate_config_file_path(self, source_path):
+        """ Construct and return a valid source_path for config file.
+        None is returned if the file doesn't exist"""
+        if source_path is "":
+            # if no source_path provided, set current
+            # working directory
+            path = os.getcwd()
+        elif not os.path.isabs(source_path):
+            # make source_path absolute
+            path = os.path.abspath(source_path)
+        else:
+            # normalize in other cases
+            path = os.path.normpath(source_path)
+        if os.path.isdir(path):
+            # append config filename
+            path = path + '/.p4clean'
+        if not os.path.exists(path):
+            if source_path is not "":
+                # config file sets by user can't be found.Throw an error
+                self.logger.error("Config file does'nt exist: %s", source_path)
+                raise IOError
+            else:
+                return None
+        return path
+
+    def parse_config_file(self, path):
+        config = ConfigParser.RawConfigParser()
+        try:
+            config.read(path)
+            exclusion_list = config.get(P4CleanConfig.SECTION_NAME,
+                                        P4CleanConfig.EXCLUSION_OPTION)
+            return exclusion_list.split(';')
+        except IOError:
+            # No .p4clean find. That's okay.
+            return []
+        except ConfigParser.NoSectionError:
+            print "Error: Invalid p4clean config file: No section named \"%s\" found." % \
+                P4CleanConfig.SECTION_NAME
+            return []
+        except ConfigParser.NoOptionError:
+            print "Error: Invalid p4clean config file: No option named \"%s\" found." % \
+                P4CleanConfig.EXCLUSION_OPTION
+            return []
 
 
 def delete_empty_folders(root):
@@ -16,7 +93,7 @@ def delete_empty_folders(root):
                     pass
 
 
-def get_perforce_status_on_folder(path):
+def get_perforce_status(path):
     old_path = os.getcwd()
     try:
         os.chdir(path)
@@ -27,33 +104,46 @@ def get_perforce_status_on_folder(path):
         os.chdir(old_path)
 
 
-def get_files_to_delete_from_perforce_status(status):
-    pass
-
-
-def filter_out_exluded_files_from_files_list(files_list):
-    pass
+def compute_files_to_delete(config, status):
+    status_lines = status.split('\n')
+    files_to_delete = []
+    for filename in status_lines:
+        if re.match('.*reconcile to add.*', filename):
+            filename = re.sub(r"\s-\s.*$", "", filename)
+            filename = filename.strip()
+            if not config.is_excluded(filename):
+                files_to_delete.append(filename)
+    return files_to_delete
 
 
 def delete_files(files_list):
-    pass
+    for filename in files_list:
+        os.remove(filename)
+
+
+def delete_untracked_files(config, path):
+    perforce_status = get_perforce_status(path)
+    files_to_delete = compute_files_to_delete(config, perforce_status)
+    delete_files(files_to_delete)
 
 
 def main():
-    """The main"""
+    # TODO:
+    # push to github
+    # improved setup.py and write readme.rst
+    # add good logs
+    # test this thing on a real folder
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "path", help="root path from which all child empty folders will be deleted")
+    parser.add_argument("path",
+                        help="root path from which all child empty folders will be deleted")
+    parser.add_argument("exclude",
+                        help="files exclusion pattern (e.g.: *.txt")
     args = parser.parse_args()
 
-    # get the diff between server and local
-    perforce_status = get_perforce_status_on_folder(args.path)
+    config = P4CleanConfig(args.path)
 
-    files_to_delete = get_files_to_delete_from_perforce_status(perforce_status)
+    delete_untracked_files(config, args.path)
 
-    delete_files(files_to_delete)
-
-    # Remove any empty folders left by the clean up
     delete_empty_folders(args.path)
 
 
--- test/test_p4clean.py
+++ test/test_p4clean.py
@@ -1,7 +1,18 @@
 import pytest
 import shutil
 import os
+import itertools
 import p4clean
+from minimock import mock
+from minimock import restore
+import ConfigParser
+import __builtin__
+
+
+def unused():
+    """ unused function to make linter complain about unused import"""
+    ConfigParser()
+    __builtin__.open()
 
 
 def test_delete_empty_folders():
@@ -70,27 +81,123 @@ def test_delete_empty_folder_keep_current_folder():
     shutil.rmtree(root_folder)
 
 
-def test_get_perforce_status_on_folder():
-    pass # We don't test method because we assume that Perfoce is correct.
-    # status = p4clean.get_perforce_status_on_folder('/Users/okcompute/Developer/Perforce/p4clean')
+def test_get_perforce_status():
+    pass  # We don't test method because we assume that Perfoce is correct.
+    # status = p4clean.get_perforce_status('/Users/okcompute/Developer/Perforce/p4clean')
     # print status
 
 
-def test_get_files_to_delete_from_perforce_status():
-    fake_status = "new_folder/haha.txt - reconcile to add \n\r//depot/p4clean/new_folder/haha.txt#1
-test2.txt - reconcile to add //depot/p4clean/test2.txt#1
+def test_get_files_to_delete_with_excluded_pattern():
+    fake_status = "new_folder/haha.txt - reconcile to add \
+            //depot/p4clean/new_folder/haha.txt#1\n \
+            test.txt - reconcile to add //depot/p4clean/test.txt#1\n \
+            test.py - reconcile to add //depot/p4clean/test.py#1\n \
+            test.c - reconcile to add //depot/p4clean/test.c#1\n \
+            test.h - reconcile to add //depot/p4clean/test.h#1\n \
+            test.log - reconcile to add //depot/p4clean/test.log#1"
+
+    config = p4clean.P4CleanConfig(exclusion="*.py;*.c;*.h")
+
+    files_to_delete = p4clean.compute_files_to_delete(config, fake_status)
+
+    print files_to_delete
+    assert len(files_to_delete) == 3, "Unexpected numbers of files to delete"
+    assert "new_folder/haha.txt" in files_to_delete, "Expected file not found"
+    assert "test.txt" in files_to_delete, "Expected file not found"
+    assert "test.log" in files_to_delete, "Expected file not found"
 
 
-def test_filter_out_excluded_files_from_files_list():
-    pass
+def test_delete_files():
+    root_folder = os.tmpnam()
+
+    # Create a folder tree
+    os.mkdir(root_folder)
+    os.mkdir(root_folder + '/folderA')
+    os.mkdir(root_folder + '/folderA/folderAA')
+    os.mkdir(root_folder + '/folderB')
+
+    def create_file(root, path):
+        temp_file_path = root + '/' + path
+        temp_file = open(temp_file_path, "wb")
+        temp_file.write("")
+        temp_file.close()
+        return temp_file_path
+
+    files = ['folderA/temp.txt', 'folderA/folderAA/temp.txt', 'folderB/temp.txt']
+
+    # populate random folders with one file
+    created_files = []
+    for file in files:
+        created_files.append(create_file(root_folder, file))
+
+    # the tested function call
+    p4clean.delete_files(created_files)
+
+    files_list = [files for path, directories, files in os.walk(root_folder)]
+    # flatten the list of list to 1 dimension list
+    files_list = list(itertools.chain.from_iterable(files_list))
+
+    assert len(files_list) == 0, "All files should have been deleted"
+
+    shutil.rmtree(root_folder)
 
 
-def test_dont_delete_excluded_file():
-    pass
+def test_parse_config_file():
+    def config_read(path):
+        pass
+
+    def config_get(section, option):
+        return '*.txt;test.cpp;*.log;/folder/folder/file.x'
+
+    def open(path, args):
+        class unused:
+            def __enter__(self):
+                return None
+
+            def __exit__(self, type, value, traceback):
+                pass
+        return unused()
+
+    # def P4CleanConfig_init(self, path, exclusion):
+    #     pass
+
+    mock('ConfigParser.RawConfigParser.read', returns_func=config_read)
+    mock('ConfigParser.RawConfigParser.get', returns_func=config_get)
+    mock('__builtin__.open', returns_func=open)
+    # mock('P4CleanConfig.__init__', returns_func=P4CleanConfig_init)
+
+    config = p4clean.P4CleanConfig()
+
+    exclusion_list = config.parse_config_file('path')
+
+    # restore __builtin__.open() because the test system uses it!
+    restore()
+
+    assert '*.txt' in exclusion_list, "exclusion pattern should be in list"
+    assert 'test.cpp' in exclusion_list, "exclusion pattern should be in list"
+    assert '*.log' in exclusion_list, "exclusion pattern should be in list"
+    assert '/folder/folder/file.x' in exclusion_list, "exclusion pattern should be in list"
 
 
-def test_dont_delete_excluded_pattern():
-    pass
+def test_parse_config_file_constructor():
+    """ Test the construction of a P4CleanConfig object. Note: this uses a sample
+    .p4clean file located in /test/data folder"""
+    config = p4clean.P4CleanConfig('./test/data')
+
+    assert config.is_excluded('test.txt'), "File should be exluded"
+    assert config.is_excluded('abracadabra.log'), "File should be exluded"
+    assert config.is_excluded('/folder/folder/test.txt'), "File should be exluded"
+    assert config.is_excluded('/folder/folder/abracadabra.log'), "File should be exluded"
+    assert config.is_excluded('test.cpp'), "File should be exluded"
+    assert not config.is_excluded('/folder/test.cpp'), "File should be exluded"
+    assert not config.is_excluded('file.x'), "File should not be exluded"
+    assert config.is_excluded('/folder/folder/file.x'), "File should be exluded"
+    assert config.is_excluded('.git/'), "Directory should be exluded"
+    assert config.is_excluded('/folder/x/.git/file.x'), "File should be exluded"
+    assert config.is_excluded('.ctags'), "File should be exluded"
+    assert config.is_excluded('/tata/tata/.ctags'), "File should be exluded"
+    assert config.is_excluded('.vimrc'), "File should be exluded"
+    assert config.is_excluded('/tata/tata/.vimrc'), "File should be exluded"
 
 
 pytest.main()
