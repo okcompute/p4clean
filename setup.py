793142189acb7b348801b8d9542521436cf634e5
Pascal Lalancette <okcompute@me.com>
2013-03-25 21:54:11 -0400

    Updated distribution files. I used autopep8 as template/guideline

--- setup.py
+++ setup.py
@@ -1,10 +1,43 @@
-from distutils.core import setup
+# !/usr/bin/env python
+# # -*- coding: utf-8 -*-
+# """Setup for p4clean."""
 
-setup(name="P4Clean",
-      version="1.0",
-      description="Perforce utilities",
-      author="Pascal Lalancette",
-      author_email="okcompute@icloud.com",
-      url="",
-      py_modules=['p4clean'],
-      )
+import ast
+from setuptools import setup
+
+
+
+def version():
+    """Return version string."""
+    with open('p4clean.py') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+with open('README.rst') as readme:
+    setup(
+        name='p4clean',
+        version=version(),
+        description='A tool to reset perforce local workspace to its initial state. '
+        'The operation is similar to "Reconcile Offline Work" operation with more '
+        'features.',
+        long_description=readme.read(),
+        license='Expat License',
+        author='Pascal Lalancette',
+        author_email='okcompute@gmail.com',
+        url='https://github.com/okcompute/p4clean',
+        classifiers=[
+            'Development Status :: 1 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python'
+            ],
+        keywords='perforce, clean, initial',
+        test_suite='test.test_p4clean',
+        py_modules=['p4clean'],
+        zip_safe=False,
+        entry_points={'console_scripts': ['p4clean = p4clean:main']},
+        )
