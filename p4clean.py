f2b8d2c4d336b2564f55e76693b09a8238ed0a9a
J.T. Conklin <jtc@acorntoolworks.com>
2014-03-05 20:18:15 -0800

    Remove untracked symlinks to directories.

--- p4clean.py
+++ p4clean.py
@@ -119,6 +119,15 @@ class Perforce(object):
                 if platform.system() == 'Windows':
                     local_file = local_file.lower()
                 local_files.append(local_file)
+	    # os.walk() treats symlinks to directories as if they 
+	    # are directories, but we need to treat them as files.
+	    for directory in directories:
+		local_file = os.path.join(path, directory)
+		if os.path.islink(local_file):
+		    if platform.system() == 'Windows':
+			local_file = local_file.lower()
+		    local_files.append(local_file)
+		
         untracked_files = set(local_files) - set(depot_files)
         return list(untracked_files)
 
